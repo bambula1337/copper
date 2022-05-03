a3acd0dc52835b75aaecd9b936d7ba821f6475ce
Adm's <lemopidr1@gmail.com>
2022-05-03 15:23:35 +0200

    Initialized project

---
+++ copper-vue-2/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <p>home page</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  components: {
+   
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
