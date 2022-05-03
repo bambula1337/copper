10268c48754b89eab4285f6c96d226b6408ce4e2
Adm's <lemopidr1@gmail.com>
2022-05-03 15:38:22 +0200

    Added Tailwind CSS

--- copper-vue-2/src/App.vue
+++ copper-vue-2/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <router-view />
+    <div class="main">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  
+ 
 }
 
 
--- copper-vue-2/src/main.js
+++ copper-vue-2/src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+//Tailwind CSS Connection
+import "tailwindcss/tailwind.css";
 
 Vue.config.productionTip = false;
 
